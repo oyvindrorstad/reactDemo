de6e9499e72295d4d549e17d841b1a39ec68f9ad
oyvindrorstad <oyvindrorstad@gmail.com>
2021-10-11 20:31:16 +0200

    Initial commit

---
+++ src/test/java/rorstad/oyvind/reactDemo/ReactDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rorstad.oyvind.reactDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
